a341f4e7442dbd51dade25cbd92931d363717a5c
altaj1 <altaj1019@gmail.com>
2024-08-01 01:33:09 +0600

    add: swiper slider & sweetalert2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
-import StoreProvider from "./StoreProvider";
+import StoreProvider from "./Providers/StoreProvider";
+import QueryProvider from "./Providers/QueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,10 +15,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <StoreProvider>
+       <QueryProvider>
+       <StoreProvider>
         <Navbar></Navbar>
         {children}
         </StoreProvider>
+       </QueryProvider>
         </body>
     </html>
   );
