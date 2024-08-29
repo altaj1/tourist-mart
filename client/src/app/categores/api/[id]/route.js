e173b552e2309c06400bb176a28f4f0592d3d493
altaj1 <altaj1019@gmail.com>
2024-08-29 20:13:54 +0600

    get sopping cart product length

--- client/src/app/categores/api/[id]/route.js
+++ client/src/app/categores/api/[id]/route.js
@@ -30,18 +30,5 @@ export const GET = async (request, {params})=>{
     } catch (error) {
         console.log(error)
     }
-    // Log the parsed query parameters for debugging
-    
-console.log(categoresProduct)
-    // You can now use these values to query the MongoDB collection
-    // Example: Find products based on the category and product name
-    // const query = {};
-    // if (categoresItem) {
-    //   query.categoresItem = categoresItem;
-    // }
-    // if (categoresProduct) {
-    //   query.categoresProduct = categoresProduct;
-    // }
-
-    return NextResponse.json({massge:" data not forund"})
+ 
 }
