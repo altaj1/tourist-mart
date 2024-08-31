efd6d56a1f1a7705858ff7a2d609a19152d2df9d
altaj1 <altaj1019@gmail.com>
2024-08-31 22:42:45 +0600

    get dada from details page

--- client/src/app/product-details/[id]/page.jsx
+++ client/src/app/product-details/[id]/page.jsx
@@ -1,6 +1,19 @@
+"use client"
+import useAxiosSecure, { axiosSecure } from '@/lib/hooks/apiHooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-
-const page = () => {
+const page = ({params}) => {
+    // console.log(params.id)
+    const axiosS= useAxiosSecure()
+    const {data:productDetails={}} = useQuery({
+        queryKey:["detailsProduct"],
+        queryFn: async()=>{
+            const {data} = await axiosSecure(`/product-details/api/${params.id}`)
+         
+            return data?.data
+        }
+    })
+    console.log(productDetails)
     return (
         <div>
             this is product details
